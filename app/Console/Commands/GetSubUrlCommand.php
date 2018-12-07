d85d37e547d61c63558ae9484bdd32aa61ef0f3d
jixingmin <jixingmin@e-business.co.jp>
2018-12-07 10:27:09 +0900

    除外URL調整

--- app/Console/Commands/GetSubUrlCommand.php
+++ app/Console/Commands/GetSubUrlCommand.php
@@ -13,7 +13,7 @@ class GetSubUrlCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:getsuburl {baseurl}';
+    protected $signature = 'command:getsuburl {baseurl?}';
 
     /**
      * The console command description.
@@ -40,12 +40,40 @@ class GetSubUrlCommand extends Command
     public function handle()
     {
         $baseurl = $this->argument("baseurl");
-        $this->parserLinks($baseurl);
+
+        $destination_depth = 3;
+
+        if (empty($baseurl)) {
+
+            $url_links = DB::select('select link, depth from link_urls where done_flg = ? order by depth', [0]);
+            foreach($url_links as $link) {
+                $depth = $link->depth;
+                $client = new Client();
+
+                // 深さnまで掘出
+                while ($destination_depth > 0) {
+                    $this->info("深さ[${depth}] 掘出 開始");
+
+                    $url_links = DB::select('select link, text from link_urls where depth = ? and done_flg = ?', [$depth++, 0]);
+                    foreach($url_links as $link) {
+                        $this->getSubUrls($client, $link->link, $depth);
+                    }
+
+                    $this->info("掘出 終了");
+                    sleep(10);
+
+                    $destination_depth--;
+                }
+            }
+        }
+        
+
+        $this->parserLinks($baseurl, $destination_depth);
         $this->info("処理完了");
         //
     }
 
-    public function parserLinks($base_url) {
+    public function parserLinks($base_url, $destination_depth) {
 
         $depth = 0;
         DB::insert('INSERT INTO link_urls(link, depth) VALUES (?, ?)', [$base_url, $depth++]);
@@ -54,11 +82,10 @@ class GetSubUrlCommand extends Command
         $suburls = $this->getSubUrls($client, $base_url, $depth);
 
         // 深さnまで掘出
-        $destination_depth = 3;
-        while ($destination_depth > 0) {
+        while (!empty($suburls) && $destination_depth > 0) {
             $this->info("深さ[${depth}] 掘出 開始");
 
-            $url_links = DB::select('select link, text from link_urls where depth = ?', [$depth++]);
+            $url_links = DB::select('select link, text from link_urls where depth = ? and done_flg = ?', [$depth++, 0]);
             foreach($url_links as $link) {
                 $this->getSubUrls($client, $link->link, $depth);
             }
@@ -72,6 +99,16 @@ class GetSubUrlCommand extends Command
 
     public function getSubUrls($client, $url, $depth) {
         $patterns = array('a');
+
+        $response = @file_get_contents($url);
+        if ($response === false) {
+            $this->error("無効なリンクURL[ $url ]");
+            DB::update('update link_urls set done_flg = ? where link = ?', [1, $url]);
+            return null;
+        }
+        
+        sleep(10);
+
         $crawler = $client->request('GET', $url);
         $subUrls = array();
         foreach($patterns as $pattern) {
@@ -82,6 +119,13 @@ class GetSubUrlCommand extends Command
                 // sleep(1);
                 if ($strSize > 4) {
                     $doUrl = $node->link()->getUri();
+                    $excludeStrArray = array('mailto:', 'tel:', 'javascript:');
+                    foreach($excludeStrArray as $value) {
+                        if (strpos($doUrl, $value) !== false) {
+                            return;
+                        }
+                    }
+
                     // error_log('before:<'.$node->link()->getUri().'>after<'.$node->attr('href').'>');
                     // home pageだけなら除外する
                     $urlPath = parse_url($doUrl, PHP_URL_PATH);
@@ -89,7 +133,7 @@ class GetSubUrlCommand extends Command
                     $path = explode(".", $urlPath);
                     $last = end($path);
                     $excludeSuffixArray = array('js', 'css', 'jpg', 'jpeg', 'gif', 'bmp', 'png', 'txt');
-                    if ($urlPath !== '/' && !in_array($last, $excludeSuffixArray)) {
+                    if ($urlPath !== '/' && !in_array($last, $excludeSuffixArray) && strpos($doUrl, 'mailto:') === false && strpos($doUrl, 'tel:') === false && strpos($doUrl, 'javascript:') === false) {
                         return array('text'=>$node->text(), 'url'=>$doUrl);
                     }
                 }
@@ -103,7 +147,7 @@ class GetSubUrlCommand extends Command
                 $subUrls[$val['text']] = $val['url'];
                 $url_exist = DB::select('select count(link) as exist from link_urls where link = ?', [$val['url']]);
 
-                if (empty($url_exist) || empty($url_exist->exist)) {
+                if (empty($url_exist[0]->exist)) {
                     DB::insert('INSERT INTO link_urls(link, text, depth) VALUES (?, ?, ?)', [$val['url'], $val['text'], $depth]);
 
                     $sleep_count++;
@@ -113,7 +157,7 @@ class GetSubUrlCommand extends Command
                     }
                 } 
             }
-            DB::update('update link_urls set have_link_counts = ? where link = ? and depth = ?', [count($subUrls), $url, $depth-1]);
+            DB::update('update link_urls set have_link_counts = ?, done_flg = ? where link = ? and depth = ?', [count($subUrls), 1, $url, $depth-1]);
         }
         return $subUrls;
     }
